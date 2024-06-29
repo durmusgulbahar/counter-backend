a06ab04fa8383af5133a585837e04dfd3aef7744
durmusgulbahar <durmus.gulbahar@outlook.com>
2024-06-29 21:15:30 +0000

    init

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+// src/index.ts
+
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, TypeScript with Node.js!\n');
+});
+
+server.listen(3000, '127.0.0.1', () => {
+  console.log('Server running at http://127.0.0.1:3000/');
+});
